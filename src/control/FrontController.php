adfd4d7faf8dc3e7d5ab90c35115680aec10aa07
flavio <flavio@flavio>
2016-10-30 23:30:35 -0200

    doc comments and include of solve alias method

--- src/control/FrontController.php
+++ src/control/FrontController.php
@@ -1,31 +1,136 @@
 <?php
 namespace curriculum\control;
 
-header('Content-Type: text/html; charset=utf-8');
-require '../../vendor/autoload.php';
+// require file that load the dependencies
+require_once  '../../vendor/autoload.php';
 
 use ttm\control\AbstractFrontController;
+use ttm\util\TTMException;
+use curriculum\Config;
 
+/**
+ * @author flaviodev - Flávio de Souza TTM/ITS - fdsdev@gmail.com
+ *
+ * Class FrontController - Implements AbstractFrontController encapsulating the treatment of
+ * service and command requests.
+ * 
+ * @see \ttm\control\AbstractFrontController
+ * 
+ * @package curriculum-control-php
+ * @namespace curriculum\control
+ * @version 1.0
+ */
 class FrontController extends AbstractFrontController {
+	
+	/**
+	 * @method locateService - Implements the seek of the registred services
+	 *
+	 * @param $serviceInterfaceName - service interface name for locating
+	 * @return the instance of required service
+	 *
+	 * @throws InvalidArgumentException - The service interface name can't be null
+	 * @throws InvalidArgumentException - No service registred for this service interface
+	 * @throws TTMException - Error on instance of the service
+	 *
+	 * @access public
+	 * @since 1.0
+	 */
+	public function locateService($serviceInterfaceName) {
+		if(is_null($serviceInterfaceName)) {
+			throw new \InvalidArgumentException("The service interface name can't be null");
+		}
+		
+		// getting registered services
+		$services = Config::getConfig()->services; 
+		
+		//whether service is not found
+		if(!isset($services[$serviceInterfaceName])) {
+			throw new \InvalidArgumentException("No service registered for this service interface");
+		}
+		
+		$serviceName = $services[$serviceInterfaceName];
+		
+		$messageError = "Error on instance of the service: ".$serviceName;
+		$service = null;
+		try {
+			$service = new $serviceName();
+		} catch (\Exception $e) {
+			throw new TTMException($messageError);
+		}
+		
+		if(is_null($service)) {
+			throw new TTMException($messageError);
+		}
+		
+		return $service;
+	}
 
-	
-	public function locateController($interfaceName) {
-		$interfaceName='curriculum\\control\\services\\'.$interfaceName;
-		$controllerName = $this->getControllerImplementation($interfaceName);
-		return new $controllerName();
+	/**
+	 * @method solveServiceInterfaceAlias - Implements the solve of the service alias
+	 *
+	 * @param $serviceInterfaceAlias - service interface alias for solving
+	 * @return the service interface name
+	 *
+	 * @throws InvalidArgumentException - The service interface alias can't be null
+	 *
+	 * @access public @abstract
+	 * @since 1.0
+	 */
+	public function solveServiceInterfaceAlias($serviceInterfaceAlias) {
+		if(is_null($serviceInterfaceAlias)) {
+			throw new \InvalidArgumentException("The service interface alias can't be null");
+		}
+		
+		return 'curriculum\\control\\services\\'.$serviceInterfaceAlias;
+		
 	}
-	
-	public function locateCommand($commandName) {
-		$commandName = $this->getCommandsImplementation($commandName);
-		return new $commandName();
+
+	/**
+	 * @method locateCommand - Implements the seek of the registred commands
+	 *
+	 * @param $commandAlias - command alias for locating
+	 * @return the instance of required command
+	 *
+	 * @throws InvalidArgumentException - The command alias can't be null
+	 * @throws InvalidArgumentException - No command registred for this command alias
+	 * @throws TTMException - Error on instance of the command
+	 *
+	 * @access public
+	 * @since 1.0
+	 */
+	public function locateCommand($commandAlias) {
+		if(is_null($commandAlias)) {
+			throw new \InvalidArgumentException("The command alias name can't be null");
+		}
+		
+		//getting the registered commands
+		$commands = Config::getConfig()->commands;
+		
+		// whether command not found
+		if(!isset($commands[$commandAlias])) {
+			throw new \InvalidArgumentException("No command registred for this command alias");
+		}
+		
+		$commandName = $commands[$commandAlias];
+		
+		$messageError = "Error on instance of the command: ".$commandName;
+		$command = null;
+		try {
+			$$command = new $commandName();
+		} catch (\Exception $e) {
+			throw new TTMException($messageError);
+		}
+		
+		if(is_null($command)) {
+			throw new TTMException($messageError);
+		}
+		
+		return $command;
 	}
-	
-	public function getConfigInfo() {
-		return Config::getConfig();
-	}
-	
 }
 
+// when .htaccess redirects the request, the front controller is created and 
+// the processeRequest method is invoked 
 $frontController = new FrontController();
 $frontController->processRequest();
 
