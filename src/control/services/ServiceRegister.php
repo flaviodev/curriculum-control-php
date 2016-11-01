d3a7ae6415f7ef4c7d51d0c26c3ccb3e1e3455c1
flavio <flavio@flavio>
2016-11-01 16:15:10 -0200

    service alteration

--- src/control/services/ServiceRegister.php
+++ src/control/services/ServiceRegister.php
@@ -3,31 +3,5 @@
 namespace curriculum\control\services;
 
 interface  ServiceRegister {
-	public function getProfiles();
-	
-	public function getProfile(int $profileId);
-
-	public function createProfile($profile);
-	
-	public function updateProfile($profile);
-	
-	public function deleteProfile(int $profileId);
-	
-	public function getAcademicsQualifications($profileId);
-	
-	public function addAcademicQualifications($qualification);
-	
-	public function updateAcademicQualifications($qualification);
-	
-	public function removeAcademicQualifications(int $qualificationId);
-	
-	public function getInstitutesOfEducation();
-	
-	public function getInstituteOfEducation(int $instituteId);
-	
-	public function createInstituteOfEducation($instituteId);
-	
-	public function updateInstituteOfEducation($institute);
-	
-	public function deleteInstituteOfEducation(int $instituteId);
+	public function publishCurriculum($profileId);
 }
--- src/control/services/ServiceRegisterImp.php
+++ src/control/services/ServiceRegisterImp.php
@@ -2,74 +2,18 @@
 
 namespace curriculum\control\services;
 
-use curriculum\model\AcademicQualifications;
-use curriculum\model\InstituteOfEducation;
-use curriculum\model\Profile;
-use ttm\control\ServiceHelper;
 use curriculum\Config;
+use ttm\control\ServiceHelper;
 
 class ServiceRegisterImp implements ServiceRegister {
 	private $helper;
 	
 	public function __construct() {
-		$config = Config::getConfigDB();
+		$config = Config::getDaoConfig();
 		$this->helper = new ServiceHelper($config);
 	}
-	
-	public function getProfiles():array {
-		return $this->helper->getEntities(Profile::class);
-	}
-	
-	public function getProfile(int $profileId) {
-		return $this->helper->getEntity(Profile::class, $profileId);
-	}
 
-	public function createProfile($profile) {
-		return $this->helper->createNewEntity(Profile::class, $profile);
-	}
-	
-	public function updateProfile($profile) {
-		$this->helper->updateEntity(Profile::class, $profile);
-	}
-
-	public function deleteProfile(int $profileId) {
-		$this->helper->deleteEntity(Profile::class,$profileId);
-	}
-
-	public function getAcademicsQualifications($profileId) {
-		$profile = $this->helper->getEntity(Profile::class, $profileId);
-		return  $profile->getAcademicsQualifications();
-	}
-
-	public function addAcademicQualifications($qualification) {
-		return $this->helper->createNewEntity(AcademicQualifications::class, $qualification);
-	}
-
-	public function updateAcademicQualifications($qualification) {
-		$this->helper->updateEntity(AcademicQualifications::class, $qualification);
-	}
-	
-	public function removeAcademicQualifications(int $qualificationId) {
-		$this->helper->deleteEntity(AcademicQualifications::class,$qualificationId);
-	}
-	
-	public function getInstitutesOfEducation() {
-		return $this->helper->getEntities(InstituteOfEducation::class);
-	}
-	
-	public function getInstituteOfEducation(int $instituteId) {
-		return $this->helper->getEntity(InstituteOfEducation::class, $instituteId);
-	}
-	
-	public function createInstituteOfEducation($institute) {
-		return $this->helper->createNewEntity(InstituteOfEducation::class, $institute);
-	}
-	
-	public function updateInstituteOfEducation($institute) {
-		$this->helper->updateEntity(InstituteOfEducation::class, $institute);
-	}
-	
-	public function deleteInstituteOfEducation(int $instituteId) {
-		$this->helper->deleteEntity(InstituteOfEducation::class, $instituteId);
+	public function publishCurriculum($profileId){
+		return "curriculum published";
 	}
 }
